5d230f7508cff274bfb4ffd90c9525c6806ef978
0x0cc <chichu79@163.com>
2022-02-04 23:37:18 +0800

    building1.0.5a

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -46,14 +46,14 @@ Page({
     var distance = endX-startX;
     console.log(distance)
     // 负数为向左 正数向右
-    if(distance >= 100) {
+    if(distance >= 80) {
       this.setData({
         current: 1,
         slide_block: 'top_slide_block_right'
       })
       console.log("切换到最热")
     }
-    else if(distance <= -100) {
+    else if(distance <= -80) {
       this.setData({
         current: 0,
         slide_block: 'top_slide_block_left'
